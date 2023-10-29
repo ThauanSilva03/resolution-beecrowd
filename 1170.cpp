41dadcfab6083e259a9de5228384a515ee28dc89
ThauanSilva03 <145589784+ThauanSilva03@users.noreply.github.com>
2023-10-29 13:56:18 -0300

    Create 1170.cpp

---
+++ 1170.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int qnt;
+    double sup;
+    cin >> qnt;
+    while(qnt!=0){
+        int i = 0;
+        cin >> sup;
+        while(sup > 1){
+            sup = sup/2;
+            i++;
+        }
+        cout << i << " dias\n";
+        qnt--;
+    }
+
+    return 0;
+}
