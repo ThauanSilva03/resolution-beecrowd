9849fdb8dfe34d71c23d41cedc50b5f090aeb76a
ThauanSilva03 <145589784+ThauanSilva03@users.noreply.github.com>
2023-10-29 15:15:50 -0300

    G 2374.cpp

---
+++ 2374.cpp
@@ -0,0 +1,9 @@
+//G
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int pressaoDesejada,pressaoAtual;
+    scanf("%d %d",&pressaoDesejada,&pressaoAtual);
+    printf("%d\n",pressaoDesejada - pressaoAtual);
+}
